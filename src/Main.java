19f9d01df64bd0d4c215314b5936d0e1cf19088b
Zoli <heramost@gmail.com>
2018-12-11 23:46:55 +0100

    day 7 - first part

--- src/Main.java
+++ src/Main.java
@@ -1,58 +1,94 @@
-import java.awt.*;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.*;
-import java.util.List;
-
-import javafx.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String args[]) {
         List<String> input = readInput();
-        List<Point> points = new ArrayList<>();
-        input.forEach(str -> {
-            String[] parsedInput = str.split(", ");
-            points.add(new Point(Integer.valueOf(parsedInput[0]), Integer.valueOf(parsedInput[1])));
-        });
-        double maxDistance = 0;
-        for (int i = 0; i < points.size() - 1; ++i) {
-            for (int j = i + 1; j < points.size(); ++j) {
-                double currentDistance = points.get(i).distance(points.get(j));
-                if (maxDistance < currentDistance) {
-                    maxDistance = currentDistance;
-                }
-            }
+        Map<String, Node> nodes = new HashMap<>();
+        for (String str : input) {
+            String[] splitStr = str.split(" ");
+            Node parentNode = getNode(nodes, splitStr[7]);
+            Node childNode = getNode(nodes, splitStr[1]);
+            nodes.put(parentNode.getCode(), parentNode);
+            nodes.put(childNode.getCode(), childNode);
+            childNode.getParents().add(parentNode);
+            parentNode.getChilder().add(childNode);
         }
-        int maxDistanceRoundedUp = (int) Math.ceil(maxDistance);
-        Double[][] table = new Double[maxDistanceRoundedUp][maxDistanceRoundedUp];
-        for (int i = 0; i < maxDistanceRoundedUp; ++i) {
-            for (int j = 0; j < maxDistanceRoundedUp; ++j) {
-                for (Point point : points) {
-                    double currentDistance = getDistance(point, new Point(i, j));
-                    Double sumOfDistance = Optional.ofNullable(table[i][j]).orElse(0d) + currentDistance;
-                    table[i][j] = sumOfDistance;
-                    if (sumOfDistance > 10000) {
-                        continue;
-                    }
-                }
-            }
-        }
-
-        int c = 0;
-        for (int i = 0; i < maxDistanceRoundedUp; ++i) {
-            for (int j = 0; j < maxDistanceRoundedUp; ++j) {
-                if (table[i][j] < 10000) {
-                    ++c;
-                }
-            }
-        }
-        System.out.println(c);
+        Node root = nodes.entrySet().stream()
+                .filter(e -> e.getValue().getChilder().isEmpty())
+                .map(Map.Entry::getValue)
+                .findFirst()
+                .orElse(null);
+        System.out.print(root.code);
+        printTree(root, new HashSet<>(), new ArrayList<>());
     }
 
-    private static double getDistance(Point point, Point otherPoint) {
-        return Math.abs(point.getX() - otherPoint.getX()) + Math.abs(point.getY() - otherPoint.getY());
+    private static void printTree(Node node, Set<Node> shownNodes, List<Node> waitingNodes) {
+        shownNodes.add(node);
+        waitingNodes.addAll(node.getParents().stream()
+                .filter(parentNode -> !shownNodes.contains(parentNode) && shownNodes.containsAll(parentNode.childer))
+                .collect(Collectors.toList()));
+        Node nextNode = waitingNodes.stream().min(Comparator.comparing(Node::getCode)).orElse(null);
+        if (nextNode != null) {
+            System.out.print(nextNode.getCode());
+            waitingNodes.remove(nextNode);
+            printTree(nextNode, shownNodes, waitingNodes);
+        }
     }
+
+    public static Node getNode(Map<String, Node> nodes, String code) {
+        Node node = nodes.get(code);
+        if (node == null) {
+            node = new Node(code);
+        }
+        return node;
+    }
+
+    public static class Node {
+        private String code;
+        private List<Node> parents = new ArrayList<>();
+        private List<Node> childer = new ArrayList<>();
+
+        public Node(String code) {
+            this.setCode(code);
+        }
+
+        public String getCode() {
+            return code;
+        }
+
+        @Override
+        public String toString() {
+            return "Node{" +
+                    "code='" + getCode() + '\'' +
+                    ", parents=" + getParents() +
+                    '}';
+        }
+
+        public void setCode(String code) {
+            this.code = code;
+        }
+
+        public List<Node> getParents() {
+            return parents;
+        }
+
+        public void setParents(List<Node> parents) {
+            this.parents = parents;
+        }
+
+        public List<Node> getChilder() {
+            return childer;
+        }
+
+        public void setChilder(List<Node> childer) {
+            this.childer = childer;
+        }
+    }
+
 
     public static List<String> readInput() {
         List<String> ret = new ArrayList<>();
